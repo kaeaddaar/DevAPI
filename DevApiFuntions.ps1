33bd07aded2fdb30e5df108eb74c4a77f0047144
kaeaddaar <playinfc@hotmail.ca>
2017-03-03 14:30:18 -0800

    Organized into two files and sections so we can work on this in tandem

--- DevApiFuntions.ps1
+++ DevApiFuntions.ps1
@@ -3,9 +3,18 @@
 #Fancy line of code used by azure automation ISE add-on
 #$RunAsConnection = Get-AutomationConnection -Name AzureRunAsConnection;try {$Login=Add-AzureRmAccount -ServicePrincipal -TenantId $RunAsConnection.TenantId -ApplicationId $RunAsConnection.ApplicationId -CertificateThumbprint $RunAsConnection.CertificateThumbprint -ErrorAction Stop}catch{Sleep 10;$Login=Add-AzureRmAccount -ServicePrincipal -TenantId $RunAsConnection.TenantId -ApplicationId $RunAsConnection.ApplicationId -CertificateThumbprint $RunAsConnection.CertificateThumbprint};Select-AzureRmSubscription -SubscriptionId $RunAsConnection.SubscriptionID
 # ----- starting -----
-    
+Import-Module .\Create-Vm.psm1
+Get-Command -Module Create-Vm
+
+# ----- load Other variables ----- start
 $HKeys = New-Object System.Collections.ArrayList
 $H = New-Object -TypeName hashtable
+$Sleep = 2
+$PublicIp = New-Object Microsoft.Azure.Commands.Network.Models.PSPublicIpAddress
+# ----- load Other variables ----- end
+
+
+# ----- Load Variables ----- start
 $JSON = ConvertFrom-Json -InputObject (Get-Content -Path C:\Users\cmackay\Documents\GitHub\DevAPI\Settings.json -Raw)
 $JSON.psobject.Properties | % {$H.add($_.name, $_.Value)}
 
@@ -47,8 +56,9 @@ $DataDisk_DiskSizeInGb = $H."DataDisk.DiskSizeInGb"
 $DataDisk_Lun = $H."DataDisk.Lun"
 $DataDisk_Caching = $H."DataDisk.Caching"
 $SubscriptionName = $H."SubscriptionName"
+# ----- Load Variables ----- end
 
-# ----- calculated variables -----
+# ----- load calculated variables ----- start
 $ResourceGroup_Name = $ResourceGroup_RootName + $TestNum
 $TrafficManagerEndpoint_Name = $RootName + $TestNum + "TmEndpoint"
 $TrafficManager_Name = $RootName + $TestNum + "TrafficManager"
@@ -71,11 +81,10 @@ $VhdUri_Name = $VhdUri_Name.ToLower()
 $VhdUri_BlobPath = "vhds/" + $RootName + $TestNum + "DataDisk1.vhd"
 $Storage_Name_Data1 = $RootName + $TestNum + "datastorage"
 $Storage_Name_Data1 = $Storage_Name.tolower() # Required to have in lower case
+# ----- load calculated variables ----- end
     
-# ----- Other variables -----
-$Sleep = 2
-$PublicIp = New-Object Microsoft.Azure.Commands.Network.Models.PSPublicIpAddress
 
+# ----- Get Stuff Done (GSD) -----
 if((Read-Host "Enter Y to Login") -eq "Y"){ Login-AzureRmAccount -SubscriptionName $SubscriptionName }
 
 $RG =  Get-AzureRmResourceGroup | where {$_.ResourceGroupName -eq $ResourceGroup_Name} # Returns $null if it doesn't find the resource group
@@ -92,6 +101,7 @@ $VMR = create-vm -Verbose -Subnet $Subnet -VNet $VNet -PublicIp ([ref]$PublicIP)
     -OsDisk_Uri $OsDisk_Uri -OsDisk_Name $OsDisk_Name -VM $VM
 #Catch {write-host "Create-Vm failed, breaking" +  $error[0].Exception + $error[0].FullyQualifiedErrorId; break}
     
+# ----- Add a traffix manager (post vm creation) ----- start
 # Traffic Manager
 $VM = get-azurermvm -ResourceGroupName $ResourceGroup_Name -Name $VmName # Need the VM
 $TrafficManagerProfile = Get-AzureRmTrafficManagerProfile | where {$_.Name -eq $TrafficManager_Name}
@@ -107,9 +117,9 @@ if ($TrafficManager_Name -ne $null)
     Get-AzureRmTrafficManagerEndpoint -Name $TrafficManager_Name -ResourceGroupName $ResourceGroup_Name -Type AzureEndpoints -ProfileName $TrafficManager_Name # will error if it doesn't exist
 }
 # Attach the data disks
+# ----- Add a traffix manager ----- end
 
-#$H.'VhdUri.Name' = "cmtestabc"
-#$DataStorage = New-AzureRmStorageAccount -ResourceGroupName $H.'RGName' -SkuName "Premium_LRS" -Name $H."Storage.Name" -Location $H.Location -Kind "BlobStorage" -AccessTier "Hot"
+# ----- Add data storage ----- start
 $DataStorage = Get-AzureRmStorageAccount -ResourceGroupName $ResourceGroup_Name -Name $Storage_Name
 if ($dataStorage -eq $null) { $DataStorage = New-AzureRmStorageAccount -ResourceGroupName $ResourceGroup_Name -SkuName "Premium_LRS" -Name $Storage_Name `
     -Location $Location -Kind Storage }
@@ -120,159 +130,5 @@ Add-AzureRmVMDataDisk -VM $VM -Name $VhdUri_Name -VhdUri $VhdUri -CreateOption E
 # the above worked, but I had lun and cashing parameters set, where previous attempts didn't have those set.
     
 Update-AzureRmVM -VM $VM -ResourceGroupName $ResourceGroup_Name
+# ----- Add data storage ----- end
 
-# ----- functions -----
-function Make-Nic ($Subnet, $VNet, [ref]$PublicIp, $Nic, $Subnet_Name, $Subnet_AddressPrefix, $VNet_Name, $ResourceGroup_Name, $Location, $Nic_Name, [Switch]$Verbose, [Microsoft.Azure.Commands.Compute.Models.PSVirtualMachine]$VM)
-{
-    if ($true) {write-host "Create-Subnet"}
-    $Subnet = Create-Subnet -Subnet_Name $Subnet_Name -Subnet_AddressPrefix $Subnet_AddressPrefix -Verbose
-    #Catch {write-host "Create-Subnet failed, breaking" +  $error[0].Exception + $error[0].FullyQualifiedErrorId; break}
-
-    Start-Sleep $sleep
-        
-    if ($true) {write-host "Create-VNet"}
-    $VNet = Create-VNet -ResourceGroup_Name $ResourceGroup_Name -Location $Location -Subnet_AddressPrefix $Subnet_AddressPrefix -Subnet $Subnet `
-        -VNet_Name $VNet_Name -Verbose
-    #Catch {write-host "Create-VNet failed, breaking" +  $error[0].Exception + $error[0].FullyQualifiedErrorId; break}
-
-    if ($true) {write-host "Create-PublicIp"}
-    $PublicIp = Create-PublicIp -ResourceGroup_Name $ResourceGroup_Name -Location $Location -Verbose -PublicIp ([ref]$PublicIp)
-    #Catch {write-host "Create-PublicIp failed, breaking" +  $error[0].Exception + $error[0].FullyQualifiedErrorId; break}
-
-    Start-Sleep $sleep
-
-    if ($true) {write-host "Create-Nic"}
-    $Nic = Create-Nic -ResourceGroup_Name $ResourceGroup_Name -Location $Location -PublicIp $PublicIp -Nic_Name $Nic_Name -VNet $VNet -Nic $Nic -Verbose
-    #Catch {write-host "Create-Nic failed, breaking"; break}
-
-    [Microsoft.Azure.Commands.Compute.Models.PSVirtualMachine]$VM = Add-AzureRmVMNetworkInterface -VM $VM -id $Nic.Id
-    $VM
-        
-} # Make-Nic
-
-
-function Create-Subnet ($Subnet_Name, $Subnet_AddressPrefix, [Switch]$Verbose)
-{
-    # $Subnet is used in Create-VNet
-    # Create a subnet
-<#
-    $H."Subnet.Name" = $H.VmName + "Subnet"
-    $H."Subnet.AddressPrefix" = "10.0.0.0/24"
-#>
-    $Subnet = New-AzureRmVirtualNetworkSubnetConfig -Name $Subnet_Name -AddressPrefix $Subnet_AddressPrefix
-    $Subnet
-} # Create-Subnet
-
-
-function Create-VNet ($ResourceGroup_Name, $Location, $Subnet_AddressPrefix, $VNet_Name, $Subnet, [Switch]$Verbose)
-{
-    # $VNet is used in 
-    # Create a VNet
-<#
-    $H."VNet.Name" = $H.VmName + "VNet" 
-#>
-    $VNet = New-AzureRmVirtualNetwork -Name $VNet_Name -ResourceGroupName $ResourceGroup_Name -Location $Location -AddressPrefix $Subnet_AddressPrefix -Subnet $Subnet
-    $VNet
-    Start-Sleep $sleep
-} # Create-VNet
-
-
-function Create-PublicIp ($ResourceGroup_Name, $Location, [Switch]$Verbose, [ref]$PublicIp)
-{
-    # $PublicIp used in Create-Nic
-    # Create a Public IP
-<#
-    $H."PublicIp.Name" = $H.VmName + "PublicIp"
-    $H.'PublicIp.Name' = $h.'PublicIp.Name'.ToLower()
-#>
-    ([ref]$PublicIp) = New-AzureRmPublicIpAddress -Name $PublicIp_Name -ResourceGroupName $ResourceGroup_Name -Location $Location -AllocationMethod Dynamic -DomainNameLabel $PublicIp_Name
-    $PublicIp.Value
-} # Create-PublicIp
-
-Function Create-Nic ($ResourceGroup_Name, $Location, $PublicIp, $Nic_Name, $VNet, $Nic, [Switch]$Verbose)
-{
-    # $NIC is used in Add-AzureRmVMNetworkInterface below
-    # Create a NIC
-<#
-    $H."Nic.Name" = $H.VmName + "Nic"
-#>
-    $Nic = New-AzureRmNetworkInterface -Name $Nic_Name -ResourceGroupName $ResourceGroup_Name -Location $Location -SubnetId $VNet.Subnets[0].Id -PublicIpAddressId $PublicIP.Value.Id 
-    $Nic
-} # Create-Nic
-
-
-function Create-OsDisk ($Storage_Name, $Storage_SkuName, $StorageAccount, [Microsoft.Azure.Commands.Compute.Models.PSVirtualMachine]$VM, $OsDisk_Uri, $OsDisk_Name, [Switch]$Verbose)
-{
-<#
-    $H."BlobPath" = "vhds/" + $H.'VmName' + "OsDisk1.vhd"
-#>
-    if ($true) {write-host "Create-StorageAccount"}
-    $StorageAccount = Create-StorageAccount -Storage_Name $Storage_Name -Storage_SkuName $Storage_SkuName -StorageAccount $StorageAccount -Verbose
-    #Catch {write-host "Create-StorageAccount failed, breaking" +  $error[0].Exception + $error[0].FullyQualifiedErrorId; break}
-
-    $OsDisk_Uri = $StorageAccount.PrimaryEndpoints.Blob.ToString() + $BlobPath
-<#
-    $H."OsDisk.Name" = $H.VmName + "OsDisk"
-#>
-    [Microsoft.Azure.Commands.Compute.Models.PSVirtualMachine]$VM = Set-AzureRmVMOSDisk -VM $VM -Name $OsDisk_Name -VhdUri $OsDisk_Uri -CreateOption FromImage
-    $VM
-} # Create-OsDisk
-
-
-function Create-StorageAccount ($Storage_Name, $Storage_SkuName, $StorageAccount, [Switch]$Verbose)
-{
-    # Create a storage account
-<#
-    $H."Storage.Name" = $H.VmName + "StorageAcct"
-    $H.'Storage.Name' = $H.'Storage.Name'.ToLower()
-    $H."Storage.SkuName" = "Premium_LRS" #Standard_LRS
-#>
-    #Get-AzureRmStorageAccountNameAvailability $StorageName
-    $goodName = Get-AzureRmStorageAccountNameAvailability $Storage_Name
-    write-host "$goodName.NameAvailable = """ + $goodName.NameAvailable
-    Start-Sleep 5
-    if ($goodName.NameAvailable)
-    {
-        $StorageAccount = New-AzureRmStorageAccount -ResourceGroupName $ResourceGroup_Name -Name $Storage_Name -Kind Storage -Location $Location -SkuName $Storage_SkuName
-    }
-    else
-    {
-        $StorageAccount = Get-AzureRmStorageAccount -ResourceGroupName $ResourceGroup_Name -Name $Storage_Name
-    }
-    $StorageAccount
-} #Create-StorageAccount
-
-
-function create-vm ([switch]$Verbose, $Subnet, $VNet, [ref]$PublicIp, $Nic, $StorageAccount, $VNet_Name, $Subnet_Name, $Subnet_AddressPrefix, `
-    $ResourceGroup_Name, $Location, $VmName, $VmSize, $Nic_Name, $OsDisk_Uri, $OsDisk_Name, $AutomationAccount_Name, [Microsoft.Azure.Commands.Compute.Models.PSVirtualMachine]$VM )
-{
-    #Global Inputs: $VM
-    $VM = New-AzureRmVMConfig -VMName $VmName -VMSize $VmSize -AvailabilitySetId $ASet.Id
-    #$Cred = Get-Credential -Message "Enter credentials" -UserName "cmackay"
-    #$Cred = Get-AzureRmAutomationCredential -Name "credCliff" -ResourceGroupName "cmAuto" -AutomationAccountName "cmAutomation"
-    $Cred = Get-AutomationPSCredential -Name "credCliff"
-    [Microsoft.Azure.Commands.Compute.Models.PSVirtualMachine]$VM = Set-AzureRmVMOperatingSystem -VM $VM -Windows -ComputerName $VmName -Credential $Cred -EnableAutoUpdate -TimeZone "Pacific Standard Time"
-
-<#
-    $H."Image.PublisherName" = "MicrosoftWindowsServer"
-    $H."Image.Offer" = "WindowsServer"
-    $H."Image.Skus" = "2016-Datacenter"
-    $H."Image.Version" = "latest"
-#>
-    [Microsoft.Azure.Commands.Compute.Models.PSVirtualMachine]$VM = Set-AzureRmVMSourceImage -VM $VM -PublisherName $Image_PublisherName -Offer $Image_Offer -Skus $Image_Skus -Version $Image_Version
-
-    if ($true) {Write-Host "Make-Nic"}
-    [Microsoft.Azure.Commands.Compute.Models.PSVirtualMachine]$VM = Make-Nic -Subnet $Subnet -VNet $VNet -PublicIp ([ref]$PublicIp) -Nic $Nic -Subnet_Name $Subnet_Name -Subnet_AddressPrefix $Subnet_AddressPrefix `
-        -ResourceGroup_Name $ResourceGroup_Name -Location $Location -VNet_Name $VNet_Name -Nic_Name $Nic_Name -Verbose -VM $VM
-#    Catch {write-host "Make-Nic failed, breaking" +  $error[0].Exception + $error[0].FullyQualifiedErrorId; break}
-
-    if ($true) {write-host "Create-OsDisk"}
-    $VM = Create-OsDisk -Storage_Name $Storage_Name -Storage_SkuName $Storage_SkuName -StorageAccount $StorageAccount -VM $VM -OsDisk_Uri $OsDisk_Uri `
-        -OsDisk_Name $OsDisk_Name -Verbose
-    #Catch {write-host "Create-OsDisk failed, breaking" +  $error[0].Exception + $error[0].FullyQualifiedErrorId; break}
-
-    write-host "Press C to Cancel before creating a VM"
-    if (read-host -eq "C") {break}
-    New-AzureRmVM -VM $VM -ResourceGroupName $ResourceGroup_Name -Location $Location
-    $VM
-} # Create-VM
